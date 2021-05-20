212502e6ff93dc52c38901aa8532b582f0ca6648
ShadowPam <matti-wandi@hotmail.com>
2021-05-21 01:43:16 +0200

    Basic functionality added

--- Client.java
+++ Client.java
@@ -1,21 +1,155 @@
 import java.net.Socket;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 
 public class Client {
 
     private Socket socket;
+    private Scanner clientIn;
     private String serverIP;
     private int serverPort;
 
     public Client(String serverIP, int serverPort){
         this.serverIP = serverIP;
         this.serverPort = serverPort;
+        clientIn = new Scanner(System.in);
     }
 
-    private void connect(){
+    private void connect(String usr, String pwd){
         try {
             System.out.println("Connecting to Server on " + serverIP);
             socket = new Socket(serverIP, serverPort);
             System.out.println("Connected");
+            PrintStream output = new PrintStream(socket.getOutputStream());
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            //Send login request
+            output.println("login");
+            output.flush();
+
+            //Wait for response
+            String line = input.readLine();
+
+            if(line != null){
+                //send username and password
+                output.println(usr + " " + pwd);
+                output.flush();
+
+                //Receive login success
+                line = input.readLine();
+
+                if(!line.equals("success")){
+                    return;
+                }
+                //send login success to client
+                System.out.println("Login successful");
+                System.out.println();
+
+                boolean running = true;
+
+                while(running){
+                    String[] command = clientIn.nextLine().split(" ");
+
+                    switch(command[0]){
+                        case "get":
+                            if(command[1].equals("balance")){
+                                //Send get balance req
+                                output.println("get balance");
+                                output.flush();
+
+                                //Receive current balance for client and print it
+                                line = input.readLine();
+                                System.out.println(line);
+                                break;
+                            }
+
+                        case "withdraw":
+                            int withdrawAmount;
+                            try {
+                                withdrawAmount = Integer.parseInt(command[1]);
+                            } catch (NumberFormatException e) {
+                                //Throw error to client
+                                System.out.println("Withdrawal amount not specified");
+                                break;
+                            }
+
+                            //Send withdraw request
+                            output.println("withdraw " + withdrawAmount);
+                            output.flush();
+
+                            line = input.readLine();
+                            if(line.equals("illegal")){
+                                System.out.println("You can't withdraw that much.");
+                            }else{
+                                System.out.println(line);
+                            }
+                            break;
+
+                        case "deposit":
+                            int depositAmount;
+                            try {
+                                depositAmount = Integer.parseInt(command[1]);
+                            } catch (NumberFormatException e) {
+                                //Throw error to client
+                                System.out.println("Deposit amount not specified");
+                                break;
+                            }
+
+                            //Send deposit request
+                            output.println("deposit " + depositAmount);
+                            output.flush();
+
+                            line = input.readLine();
+                            System.out.println(line);
+                            break;
+
+                        case "transfer":
+                            int transferAmount;
+                            try {
+                                transferAmount = Integer.parseInt(command[1]);
+                            } catch (NumberFormatException e) {
+                                //Throw error to client
+                                System.out.println("Transfer amount not specified");
+                                break;
+                            }
+
+                            //Send transfer
+                            output.println("transfer " + transferAmount + " " + command[2]);
+                            output.flush();
+
+                            //Receive information back and handle
+                            line = input.readLine();
+
+                            if(line.equals("e1")){
+                                System.out.println("You can't transfer that much");
+                            }else if(line.equals("e2")){
+                                System.out.println("That username does not exist.");
+                            }else{
+                                System.out.println(line);
+                            }
+                            break;
+
+                        case "logout":
+                            running = false;
+                            break;
+                    }
+                }
+
+                //Send logout request
+                output.println("logout");
+                output.flush();
+
+                System.out.println("Logging off...");
+                output.close();
+                input.close();
+                socket.close();
+                clientIn.close();
+                System.exit(0);
+            }        
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -31,11 +165,19 @@ public class Client {
     
         String ip = args[0];
         int port = Integer.parseInt(args[1]);
-    
         Client c = new Client(ip, port);
-        c.connect();
-    
-        
+
+        System.out.println("Welcome to Simple Banking App 101");
+        while(true){
+            System.out.println("Please login by following this syntax: login username passwd");
+            String[] loginReq = c.clientIn.nextLine().split(" ");
+
+            if(loginReq[0].equals("login")){
+                c.connect(loginReq[1], loginReq[2]);
+            }
+
+            System.out.println("Illegal username or password");
+        }
     }
 
 }
--- Server.java
+++ Server.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -15,13 +16,21 @@ public class Server{
     private int port;
 
     private HashMap<String, String> users;
-    private HashMap<Integer, Integer> balances;
+    private HashMap<String, Integer> balances;
     
     public Server(String ipAdress){
         this.ipAdress = ipAdress;
         running = false;
         users = new HashMap<>();
         balances = new HashMap<>();
+
+        //Add two basic users for testing purposes
+        users.put("admin", "admin");
+        users.put("foo", "bar");
+
+        //Should be extended with reading in from a file
+        balances.put("admin", 0);
+        balances.put("foo", 0);
     }
 
     private void startServer(){
@@ -43,6 +52,113 @@ public class Server{
                 Socket cSock = sSock.accept();
 
                 System.out.println("Connection established with " + cSock.getInetAddress().getHostAddress());
+                PrintStream output = new PrintStream(cSock.getOutputStream());
+                BufferedReader input = new BufferedReader(new InputStreamReader(cSock.getInputStream()));
+
+                //Client connected start handling login req
+                String line = input.readLine();
+                if(line.equals("login")){
+                    //Send back confirmation
+                    output.println("confirmed");
+                    output.flush();
+                }else{
+                    //For future in case login was not typed::when not hardcoded
+                }
+                
+                String[] usrAndPwd = input.readLine().split(" ");
+                String currentClient;
+                //Check for existing users and pwd
+                if(users.get(usrAndPwd[0]).equals(usrAndPwd[1])){
+                    //login success
+                    currentClient = usrAndPwd[0];
+                    output.println("success");
+                    output.flush();
+
+                    boolean requestLoop = true;
+                    while(requestLoop){
+                        String[] command = input.readLine().split(" ");
+
+                        switch(command[0]){
+                            case "get":
+                                if(command[1].equals("balance")){
+                                    //Send balance
+                                    output.println(balances.get(currentClient));
+                                    output.flush();
+                                    break;
+                                }
+
+                            case "withdraw":
+                                int withdrawAmount = Integer.parseInt(command[1]);
+
+                                //Try to withdraw
+                                int cBalance = balances.get(currentClient);
+
+                                if(cBalance < withdrawAmount){
+                                    //Send error
+                                    output.println("illegal");
+                                    output.flush();
+                                    break;
+                                }
+
+                                //Complete withdrawal and send success
+                                balances.put(currentClient, balances.get(currentClient)-withdrawAmount);
+                                output.println("ok!");
+                                output.flush();
+                                break;
+
+                            case "deposit":
+                                int depositAmount = Integer.parseInt(command[1]);
+
+                                //Complete deposit and send success
+                                balances.put(currentClient, balances.get(currentClient)+depositAmount);
+                                output.println("ok!");
+                                output.flush();
+                                break;
+
+                            case "transfer":
+                                int transferAmount = Integer.parseInt(command[1]);
+                                String transferUsr = command[2];
+
+                                //Check legal transfer amount
+                                if(transferAmount > balances.get(currentClient)){
+                                    //Send error
+                                    output.println("e1");
+                                    output.flush();
+                                    break;
+                                }
+
+                                //Check legal username
+                                if(!users.containsKey(command[2])){
+                                    //Send error
+                                    output.println("e2");
+                                    output.flush();
+                                    break;
+                                }
+
+                                //Send transfer
+                                balances.put(currentClient, balances.get(currentClient)-transferAmount);
+                                balances.put(transferUsr, balances.get(transferUsr)+transferAmount);
+                                output.println("ok!");
+                                output.flush();
+                                break;
+
+                            case "logout":
+                                requestLoop = false;
+                                break;
+                        }
+                    }
+                }else{
+                    output.println("Illegal login");
+                    output.flush();
+                }
+
+                
+                output.close();
+                cSock.close();
+                input.close();
+                System.out.println("Connection lost.");
+                System.out.println();
+                
             } catch (Exception e) {
                 e.printStackTrace();
             }
